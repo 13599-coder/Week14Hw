d9cfe9eab32c9f6ec652ba7c6bc8fee2f8806b7e
EvanM <13599@edu.wheatlandhigh.org>
2026-04-26 19:47:37 -0700

    Update classes.py

--- classes.py
+++ classes.py
@@ -12,3 +12,19 @@ class RPGCharacter:
       return self.__name
     def attack(self):
       print(self.__name, "attacks using", self.__attack_power)
+    def take_damage(self):
+        self.__health = self.__health - 10
+        print(self.__name, "takes damage")
+    def display_stats(self):
+        print("name:", self.__name)
+        print("health:", self.__health)
+        print("attack:", self.__attack_power)
+        print("level:", self.__level)
+class Samuri(RPGCharacter):
+    def __init__(self, name, health, attack_power, level, armor):
+        RPGCharacter.__init__(self, name, health, attack_power, level)
+        self.__armor = armor
+    def take_damage(self):
+        print(self.get_name(), "uses the armor")
+    def block(self):
+        print(self.get_name(), "blocks the attack")
